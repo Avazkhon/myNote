f9732e01a42f1e43a9b32554a93eac141443966d
Avazkhon <kamalxanovavazkhon@gmail.com>
2019-08-10 16:15:36 +0700

    wip #setting: new method new note

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { saveNote } from '../actions/index'
+import { createNewNote } from '../actions/index'
 
 
 class Main extends Component {
@@ -8,8 +8,28 @@ class Main extends Component {
 		super(props);
 		this.state = {
 			chengeTetx: '',
+			chengeTetxId: null,
+			setting: [
+				{
+					id: 1,
+					title: 'New note',
+				},
+				{
+					id: 2,
+					title: 'Open note',
+				}
+			]
 		}
+	}
 
+	componentWillReceiveProps(nexProps) {
+		const {
+			chengeTetx,
+			chengeTetxId,
+		} = this.state;
+		if (chengeTetxId) {
+			this.openNote(chengeTetxId)
+		}
 	}
 
 	handleChenge = (event) => {
@@ -19,25 +39,64 @@ class Main extends Component {
 	}
 
 	handleSave = () => {
-		const id = Date.now();
-		const text = this.state.chengeTetx;
-		this.props.saveNote(id, text)
-		this.setState({chengeTetx: ''});
+		// const id = Date.now();
+		// const text = this.state.chengeTetx;
+		// this.props.saveNote(id, text)
+		// this.setState({chengeTetx: ''});
 
 	}
 
+	handleCreateNewNote = () => {
+		const id = Date.now();
+		this.props.createNewNote(id, '')
+
+		this.setState({
+				chengeTetxId: id,
+			});
+
+		}
+
+		openNote = (id) => {
+			const note = this.props.note.find(item => {
+				return item.id === id;
+			})
+
+			this.setState({
+					chengeTetx: note.text,
+					chengeTetxId: null,
+				});
+		}
+
+
 	render () {
 		const {
-			note
+			note,
 		} = this.props;
 
 		const {
-			chengeTetx
+			chengeTetx,
+			setting,
+			chengeTetxId,
 		} = this.state;
-
 
 		return (
 			<div>
+				<div>
+					<div>Setting</div>
+						<ul>
+							{
+								setting.map((item) => {
+									return (
+										<li key={item.id} onClick={this.handleCreateNewNote}>
+											{item.title}
+										</li>
+									)
+								})
+							}
+						</ul>
+					<div>
+					</div>
+				</div>
 				<h3>text</h3>
 				<textarea name="chengeTetx" value={chengeTetx} onChange={this.handleChenge}/>
 				<input type='button' value='save' onClick={this.handleSave}/>
@@ -52,5 +111,5 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps, {
-	saveNote,
+	createNewNote,
 })(Main);
