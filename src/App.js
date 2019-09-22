6179831eb0060185db23d7a4e57c422b5a0a2af2
Avazkhon <kamalxanovavazxon@gmail.com>
2019-09-22 18:28:48 +0700

    br#3: (ndf) add test

--- src/App.js
+++ src/App.js
@@ -1,18 +1,48 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux'
 
-import Main from 'container';
 
-import store from 'store/index';
+export const doIncrement = (prevState) => ({
+  counter: prevState.counter + 1,
+});
+export const doDecrement = (prevState) => ({
+  counter: prevState.counter - 1,
+});
 
 class App extends Component {
-	render () {
-		return (
-			<Provider store={store} >
-				<Main />
-			</Provider>
-		)
-	}
+  constructor() {
+    super();
+    this.state = {
+      counter: 0,
+    };
+    this.onIncrement = this.onIncrement.bind(this);
+    this.onDecrement = this.onDecrement.bind(this);
+  }
+  onIncrement() {
+    this.setState(doIncrement);
+  }
+  onDecrement() {
+    this.setState(doDecrement);
+  }
+  render() {
+    const { counter } = this.state;
+    return (
+      <div>
+        <h1>My Counter</h1>
+        <p>{counter}</p>
+        <button
+          type="button"
+          onClick={this.onIncrement}
+        >
+          Increment
+        </button>
+        <button
+          type="button"
+          onClick={this.onDecrement}
+        >
+          Decrement
+        </button>
+      </div>
+    );
+  }
 }
-
 export default App;
