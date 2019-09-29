a89f2c7afa27e5982a8ab9f8ecd2a8a12a0cf26a
Avazkhon <kamalxanovavazxon@gmail.com>
2019-09-29 17:38:40 +0700

    br#3 add test

--- src/testCheck.spec.js
+++ src/testCheck.spec.js
@@ -1,16 +1,13 @@
 import React from 'react';
-
 import global from 'global';
 
-import App, { Counter, doIncrement, doDecrement } from './ForTestApp';
-
-describe('Local State', () => {
+import ForTestApp, { Counter, doIncrement, doDecrement } from './ForTestApp';
+const wrapper = shallow(<ForTestApp />);
+describe('test check', () => {
   it('renders the Counter wrapper', () => {
-    const wrapper = shallow(<App />);
     expect(wrapper.find(Counter)).to.have.length(1);
   });
   it('passes all props to Counter wrapper', () => {
-    const wrapper = shallow(<App />);
     let counterWrapper = wrapper.find(Counter);
     expect(counterWrapper.props().counter).to.equal(0);
     wrapper.setState({ counter: -1 });
@@ -18,13 +15,11 @@ describe('Local State', () => {
     expect(counterWrapper.props().counter).to.equal(-1);
   });
   it('increments the counter', () => {
-    const wrapper = shallow(<App />);
     wrapper.setState({ counter: 0 });
     wrapper.find('button').at(0).simulate('click');
     expect(wrapper.state().counter).to.equal(1);
   });
   it('decrements the counter', () => {
-    const wrapper = shallow(<App />);
     wrapper.setState({ counter: 0 });
     wrapper.find('button').at(1).simulate('click');
     expect(wrapper.state().counter).to.equal(-1);
