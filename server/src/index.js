75ec957ee9572fee02b4ad08b9d10791fc51ac12
Avazkhon <kamalxanovavazxon@gmail.com>
2019-12-08 20:55:52 +0700

    wip: create ./user routs

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,49 @@
 const express = require('express')
-const app = express()
+const bodyParser = require('body-parser')
+
+const app = express();
+
+app.use( bodyParser.json() );
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+const users = [
+  {
+    id: 1,
+    name: 'Vlad',
+    password: 123,
+  }
+]
 
 app.get('/', (req, res) => {
   res.status = 200;
   res.send('my note');
+});
+
+app.post('/user', (req, res) => {
+  if (req.body) {
+    res.status = 400;
+  }
+  const {
+    name,
+    password,
+  } = req.body;
+  if (name && password && name.length && password.length) {
+    users.push({
+      id: Date.now(),
+      name,
+      password
+    })
+    res.status = 200;
+  }
+  res.send();
+});
+
+app.get('/user', (req, res) => {
+  res.send(users);
 })
 
 app.listen(3000, () => {
   console.log('app my note starting!');
-})
+});
